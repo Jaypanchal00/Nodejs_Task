a3436468000af87f6f9175200b3d99a31b754f45
Jay Panchal <jkpanchal3491@gmail.com>
2025-12-05 09:42:07 +0530

    Add Movie

--- MVC/server.js
+++ MVC/server.js
@@ -1,12 +1,26 @@
-const express = require('express')
-const db = require("./config/db")
-const user = require("./model/usermodel")
-const U_router = require("./Routes/userRouter")
-const B_router = require('./Routes/BookRoutes')
-const app = express()
-app.use(express.json())
-app.use('/user', U_router)
-app.use('/Book',B_router)
+const express = require('express');
+const db = require("./config/db");
+const user = require("./model/usermodel");
+const U_router = require("./Routes/userRouter");
+const B_router = require('./Routes/BookRoutes');
+const M_router=require('./Routes/MovieRouter')
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+// Set EJS view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use('/user', U_router);
+app.use('/Book', B_router);
+app.use('./Movie',M_router)
+
+
+
 app.listen(4000, () => {
-    console.log("server listen")
-})
+    console.log("server listen");
+});
---
+++ mongo/usermodel/usermodel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+
+const userSchema = mongoose.Schema({
+    username :{
+        type:String
+
+    },
+
+    password :{
+        type:String
+    },
+
+    image:{
+        type:String
+    }
+})
+
+const usermodel = mongoose.model("user",userSchema)
+
+module.exports = usermodel
